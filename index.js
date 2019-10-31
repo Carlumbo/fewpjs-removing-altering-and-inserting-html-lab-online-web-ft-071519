4a3ebc3ecca38c398bbafae263f6e97378030c7d
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-31 18:42:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 
 
 
-
-document.querySelector("main#main").remove()
-
-const newHeader = document.createElement("h1");
-
-newHeader.id = "victory"
-
-newHeader.innerHTML = "YOUR-NAME is the champion"
